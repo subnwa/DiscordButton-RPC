1a7282c56e7dfda0935aa8af56adc5272389a5cd
nwa <nwa.bio.web@gmail.com>
2022-08-30 00:23:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ import self as self
 from setuptools import setup, find_packages
 
 auto_start = True
+error = OSError
 
 if not setup:
     raise ImportError("No setup specified")
     # if setup is not available, we need to make sure to install it
+
+
+class error(OSError):
+    def __init__(self, msg: str):
+        self.msg = msg
+        msg = "OSError: %s" % msg
+
 
 f = open("README.md")
 
