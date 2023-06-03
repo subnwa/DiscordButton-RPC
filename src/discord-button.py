bdb00d2c9a53b7b3c9e67a66d87cb371a4bffd5e
nwa <nwa.bio.web@gmail.com>
2023-06-03 14:30:02 +0300

    Fixed some redirects and confusion.
    The unimportant and unnecessary codes were cleaned and a simpler code directory was created. It is now possible to use a different text command in `class text(RPC)`. In addition, error messages are sent to the console as messages without any errors. Visual spreads have been changed and edited.

--- src/discord-button.py
+++ src/discord-button.py
@@ -2,21 +2,26 @@ from asyncio import DatagramTransport
 from cgitb import enable
 import dataclasses
 from http.client import InvalidURL
+from logging import error
 from pdb import line_prefix
+from typing import Dict, Any
 from unittest import result
 
 
-def button(label: str, url: str):
+def RPC(label: str, url: str):
     if any(v in url for v in url):
         payloads = {"label": label, "url": url}
         link = {"link": url, "label": label}
+        images = {"images": url}
+        text = {"text": url}
+        ButtonError = {"ButtonError": url}
         return button
     else:
         raise InvalidURL + TypeError("Invalid URL passed")
     pass
 
 
-class RPC(Exception):
+class button(RPC):
     def __init__(self, message: str = None):
         if message is None:
             message = 'Error: ' + message
@@ -25,11 +30,10 @@ class RPC(Exception):
     pass
 
 
-class images(Exception):
+class images(RPC):
     def __init__(self, message: str = None):
         var = (v in images for v in images)
-        image = {"image": images, "type": images.get("type")}
-        image = format(format(image, var))
+        image: dict[str, Any] = {"image": images, "type": images.get("type")}
 
     @classmethod
     def get(cls, param):
@@ -38,21 +42,15 @@ class images(Exception):
 class text(RPC):
     def __init__(self, text: str = enable):
         var = (text in result for v in text)
-        text = {"text": text}
+        text = {"text": text} != RPC(text)
         text = format(line_prefix.text)
 
 
 class ButtonError(RPC):
     def __init__(self, message: str = None):
-        super().message = message
+        error.message = message
         message = 'Error: ButtonError (RPC)'
+
 
     pass
 
-
-def button(
-        button_one_label: str = None, ):
-    if button_one_label is None:
-        raise ButtonError('None')
-
-    return
