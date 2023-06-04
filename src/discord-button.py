b389c81b47724790b0eaa2eb62e201b4e1a7b98d
nwa <nwa.bio.web@gmail.com>
2023-06-04 14:40:41 +0300

    Update discord-button.py

--- src/discord-button.py
+++ src/discord-button.py
@@ -4,6 +4,9 @@ import dataclasses
 from http.client import InvalidURL
 from logging import error
 from pdb import line_prefix
+
+import image
+from discord import message
 from typing import Dict, Any
 from unittest import result
 
@@ -12,8 +15,9 @@ def RPC(label: str , url: str):
     if any(v in url for v in url):
         payloads = {"label": label, "url": url}
         link = {"link": url, "label": label}
-        images = {"images": url}
+        images = {"images": url, "type": image.get("type") }
         text = {"text": url}
+        button = {"button": url, "message": message}
         ButtonError = {"ButtonError": url}
         return
     else:
@@ -21,27 +25,27 @@ def RPC(label: str , url: str):
     pass
 
 
-class button(RPC):
+class button():
     def __init__(self, message: str = None):
         if message is None:
             message = 'Error: ' + message
         super().message = message
 
-class images(RPC):
+class images():
     def __init__(self, message: str = None, image=  None):
         var = (v in images for v in image)
-        image: dict[str, Any] = {"image": images, "type": images.get("type")}
+        image = {"image": images, "type": images.get ("type")}
 
 
 
-class text(RPC):
+class text():
     def __init__(self, text: str = enable):
         var = (text in result for v in text)
         text = {"text": text} != RPC(text)
         format_text = format(line_prefix.text)
 
 
-class ButtonError(RPC):
+class ButtonError():
     def __init__(self, message: str = None):
         error.message = message
         message = 'Error: ButtonError (RPC)'
